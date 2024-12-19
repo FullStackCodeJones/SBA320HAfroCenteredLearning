44a5818f5d019ed7939ae675818f2640d46e289b
FullStackCodeJones <fullstackcodejones@gmail.com>
2024-12-19 15:19:25 -0500

    adding css styling and coming back later for more functionality

--- afro-centered-learning-hub/src/App.jsx
+++ afro-centered-learning-hub/src/App.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { fetchBooks } from "./api"; // Importing the function that fetches book data from the API
+import "./App.css";
 
 const App = () => {
   // State variables
   const [books, setBooks] = useState([]); // State to store the books data
+  const [filteredBooks, setFilteredBooks] = useState([]);
+  const [searchQuery, setSearchQuery] = useState(""); // State for search query
   const [loading, setLoading] = useState(true); // State to track the loading status
   const [error, setError] = useState(null); // State to store error messages
 
@@ -13,6 +16,7 @@ const App = () => {
       setLoading(true); // Set loading to true before starting the fetch process
       const booksData = await fetchBooks(); // Fetch books data from the API
       setBooks(booksData); // Update the books state with the fetched data
+      setFilteredBooks(booksData); //Initialize filteredBooks with all books
       setError(null); // Clear any previous error message
     } catch (error) {
       setError("Failed to fetch data from Open Library API"); // Set an error message if something goes wrong
@@ -25,6 +29,15 @@ const App = () => {
   useEffect(() => {
     loadBooks(); // Call the loadBooks function
   }, []); // Empty dependency array ensures this runs only once when the component is mounted
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value); // Update the search query
+    // Filter books based on title
+    const filtered = books.filter((book) =>
+      book.title.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    setFilteredBooks(filtered);
+  };
 
   // Display loading state
   if (loading) {
@@ -49,9 +62,19 @@ const App = () => {
         Education That Enriches Culture and Knowledge of Self for Brown Children
         Everywhere!
       </h1>
+      <input
+        type="text"
+        placeholder="Search By Title"
+        value={searchQuery}
+        onChange={handleSearch}
+      />
+      {filteredBooks.length === 0 && (
+        <p>No books found matching your search information.</p>
+      )}
+
       <ul>
         {/* Loop through the books and display each one */}
-        {books.map((book) => (
+        {filteredBooks.map((book) => (
           <li key={book.key}>
             <h2>{book.title}</h2>
             <p>{book.first_publish_year}</p>
